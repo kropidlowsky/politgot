4b5fecf5c224c1e2eb0b45cfe291f400ce103d00
Soib <55416118+Soib@users.noreply.github.com>
2021-03-20 20:14:39 +0100

    Themes and Drawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import Switch from '@material-ui/core/Switch';
+import Drawer from './Drawer.js'
+
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+          <Drawer />
+          <footer>
+            <Switch onClick={toggleTheme} inputProps={{ 'aria-label': 'primary checkbox' }} />
+          </footer>
+        </>
+        
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+export const lightTheme = {
+    body: '#e2e2e2',
+    text: '#363537',
+    toggleBorder: '#fff',
+    gradient: 'linear-gradient(#39598A, #79D7ED)',
+  }
+  
+  export const darkTheme = {
+    body: '#041333',
+    text: '#FFFFFF',
+    color: 'white',
+    toggleBorder: '#6B8096',
+    gradient: 'linear-gradient(#091236, #1E215D)',
+  }

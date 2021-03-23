db46a459842be41a914297b25e57a00b2ec32b32
Soib <lukasz_cybulski@icloud.com>
2021-03-23 19:34:23 +0100

    Politician List Front
    Co-Authored-By: dariusz-rodzik <60323554+dariusz-rodzik@users.noreply.github.com>

--- frontend/src/components/Politician.js
+++ frontend/src/components/Politician.js
@@ -1,34 +1,34 @@
-import React from 'react';
+// import React from 'react';
 
-// zamien list na front ktory zrobiles
-import { List } from 'antd';
+// // zamien list na front ktory zrobiles
+// import { List } from 'antd';
 
 
-const Politician = (props) => {
-    return (
-    <List
-        itemLayout="vertical"
-        size="large"
-        pagination={{
-        onChange: (page) => {
-            console.log(page);
-        },
-        pageSize: 3,
-        }}
-        dataSource={props.data}
-        renderItem={item => (
-        <List.Item>
-            <List.Item.Meta 
+// const Politician = (props) => {
+//     return (
+//     <List
+//         itemLayout="vertical"
+//         size="large"
+//         pagination={{
+//         onChange: (page) => {
+//             console.log(page);
+//         },
+//         pageSize: 3,
+//         }}
+//         dataSource={props.data}
+//         renderItem={item => (
+//         <List.Item>
+//             <List.Item.Meta 
         
-            title={item.name}
-            description={item.register}
+//             title={item.name}
+//             description={item.register}
 
-        />
+//         />
           
-        </List.Item>
-        )}
-    />
-    )
-}
+//         </List.Item>
+//         )}
+//     />
+//     )
+// }
 
-export default Politician;
+// export default Politician;
--- frontend/src/components/PoliticianList.js
+++ frontend/src/components/PoliticianList.js
@@ -1,33 +1,101 @@
 import React from 'react';
 import axios from 'axios';
-import data from '../jsons/politycy'
-
+import representatives from '../jsons/politycy'
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './politicianList.css';
+import { List, message, Avatar, Spin } from 'antd';
+import reqwest from 'reqwest';
+import InfiniteScroll from 'react-infinite-scroller';
 import Politician from './Politician';
 
 class PoliticianList extends React.Component {
-
     state = {
-        polits: []
-    }
+        data: [],
+        loading: false,
+        hasMore: true,
+      };
+    
+      componentDidMount() {
+        // this.fetchData(res => {
+          this.setState({
+            data: representatives,
+        //   });
+        });
 
-    componentDidMount() {
-        // axios.get(`http://127.0.0.1:5000/polit`)
-        //     .then(res => {
-        //         this.setState({
-        //             polits: res.data
-        //         });
-        //     })
+        
+      }
+    
+    //   fetchData = callback => {
+    //     reqwest({
+    //       url: url,
+    //       type: 'json',
+    //       method: 'get',
+    //       contentType: 'application/json',
+    //       success: res => {
+    //         callback(res);
+    //       },
+    //     });
+    //   };
+    
+      handleInfiniteOnLoad = () => {
+        let { data } = this.state;
         this.setState({
-            polits: data
-        })
-    }
+          loading: true,
+        });
+        if (data.length > 14) {
+          message.warning('Infinite List loaded all');
+          this.setState({
+            hasMore: false,
+            loading: false,
+          });
+          return;
+        }
+        this.fetchData(res => {
+          data = data.concat(res.results);
+          this.setState({
+            data,
+            loading: false,
+          });
+        });
+      };
+    
 
-    render() {
+      render() {
         return (
-            <Politician data={this.state.polits} />
-
-        )
-    }
+          <div className="infinite-container">
+            <InfiniteScroll
+              initialLoad={false}
+              pageStart={0}
+              loadMore={this.handleInfiniteOnLoad}
+              hasMore={!this.state.loading && this.state.hasMore}
+              useWindow={false}
+            >
+              <List
+                dataSource={this.state.data}
+                renderItem={item => (
+                  <List.Item key={item.id}>
+                    <List.Item.Meta
+                    //   avatar={
+                    //     <Avatar src="" />
+                    //   }
+                      title={<a href="https://ant.design">{item.name}</a>}
+                      description={item.register}
+                    />
+                    <div>.</div>
+                  </List.Item>
+                )}
+              >
+                {this.state.loading && this.state.hasMore && (
+                  <div className="load-container">
+                    <Spin />
+                  </div>
+                )}
+              </List>
+            </InfiniteScroll>
+          </div>
+        );
+      }
 }
 
 export default PoliticianList;

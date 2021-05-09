9e45bddd10dfe5f97c011fb008be133cb20102ff
kropidlowsky <kropidlowsky@protonmail.com>
2021-05-09 15:48:45 +0200

    update items

--- tools/data_collectors/goverment/representative_profiles/representative_profiles/items.py
+++ tools/data_collectors/goverment/representative_profiles/representative_profiles/items.py
@@ -8,17 +8,4 @@ import scrapy
 
 class RepresentativeProfilesItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
-    elect_date = scrapy.Field()
-    register = scrapy.Field(serialized=str)
-    votes = scrapy.Field()
-    constituency = scrapy.Field()
-    vow_date = scrapy.Field()
-    period = scrapy.Field()
-    clubs = scrapy.Field()
-
-    DOB = scrapy.Field()
-    POB = scrapy.Field()
-    education = scrapy.Field()
-    schools = scrapy.Field()
-    profession = scrapy.Field()
+    pass

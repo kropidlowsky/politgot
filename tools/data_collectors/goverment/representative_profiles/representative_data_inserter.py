67ae6b05171a3b4453f562d3030f2ff79d66d34d
kropidlowsky <kropidlowsky@protonmail.com>
2021-05-14 23:39:19 +0200

    WIP

---
+++ tools/data_collectors/goverment/representative_profiles/representative_data_inserter.py
@@ -0,0 +1,172 @@
+import datetime
+
+from representative_normalizer import RepresentativeNormalizer
+import sys
+from psycopg2 import Error
+import psycopg2
+
+
+def read_data():
+    normalizer = RepresentativeNormalizer()
+    if sys.argv == '-p':
+        normalizer.crawl_data()
+    elif sys.argv[1] == '-f':
+        normalizer.get_data_from_file(sys.argv[2])
+    else:
+        print("Unknown option")
+        exit(0)
+    normalizer.normalize()
+    return normalizer.data
+
+
+def get_db_connection():
+    connection = psycopg2.connect(user="mcpixzcwkhrrio",
+                                  password="9141440570eff0a8d538498b8a95a407ad364d9297712891184913c19192ee26",
+                                  host="ec2-54-229-68-88.eu-west-1.compute.amazonaws.com",
+                                  port="5432",
+                                  database="d704bc62gosle7")
+    return connection
+
+
+def menage_data(data: list):
+    genders = read_genders()
+    db_politicians = read_politicians()
+
+    for datum in data:
+        insert_politician(datum, genders, db_politicians)
+
+
+def read_genders():
+    result = {}
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT id, name FROM gender")
+        genders = cursor.fetchall()
+        for gender in genders:
+            result[gender[1]] = gender[0]
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        cursor.close()
+        connection.close()
+        return result
+
+
+def read_politicians():
+    result = []
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT id, name, surname, profession, picture, education, birth_place, votes, birth_date, gender FROM politicians")
+        politicians = cursor.fetchall()
+        for politician in politicians:
+            result.append({
+                'id': politician[0],
+                'name': politician[1],
+                'profession': politician[2],
+                'surname': politician[3],
+                'picture': politician[4],
+                'education': politician[5],
+                'birth_place': politician[6],
+                'votes': politician[7],
+                'birth_date': politician[8],
+                'gender': politician[9]
+            })
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        cursor.close()
+        connection.close()
+        return result
+
+
+def insert_politician(politician: dict, genders: dict, db_politicians):
+    name = politician.get('Imię')
+    surname = politician.get('Nazwisko')
+    picture = politician.get('zdjęcie')
+    dob = politician.get('Data urodzenia')
+    if dob:
+        dob = datetime.datetime.strptime(dob, '%d-%m-%Y')
+    pob = politician.get('Miejsce urodzenia')
+    gender = politician.get('Płeć')
+    profession = politician.get('Zawód')
+    votes = politician.get('Liczba głosów')
+    edu = politician.get('Wykształcenie')
+    exists = False
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        for db_politician in db_politicians:
+            if db_politician['name'] == name and db_politician['surname'] == surname:
+                exists = True
+                if not db_politician['profession']:
+                    try:
+                        cursor.execute(f"UPDATE politicians SET profession = 'profession' WHERE id = {db_politician['id']}")
+                    except (Exception, Error) as error:
+                        print("Error while updating picture", error)
+                        connection.rollback()
+                    connection.commit()
+                if not db_politician['picture']:
+                    try:
+                        cursor.execute(f"UPDATE politicians SET picture = decode('{picture.decode()}', 'base64') WHERE id = {db_politician['id']}")
+                    except (Exception, Error) as error:
+                        print("Error while updating picture", error)
+                        connection.rollback()
+                    connection.commit()
+                if not db_politician['education']:
+                    try:
+                        cursor.execute(f"UPDATE politicians SET education = '{edu}' WHERE id = {db_politician['id']}")
+                    except (Exception, Error) as error:
+                        print("Error while updating education", error)
+                        connection.rollback()
+                    connection.commit()
+                if not db_politician['birth_place']:
+                    try:
+                        cursor.execute(f"UPDATE politicians SET birth_place = '{pob}' WHERE id = {db_politician['id']}")
+                    except (Exception, Error) as error:
+                        print("Error while updating birth_place", error)
+                        connection.rollback()
+                    connection.commit()
+                if not db_politician['votes']:
+                    try:
+                        cursor.execute(f"UPDATE politicians SET votes = '{votes}' WHERE id = {db_politician['id']}")
+                    except (Exception, Error) as error:
+                        print("Error while updating votes", error)
+                        connection.rollback()
+                    connection.commit()
+                if not db_politician['birth_date']:
+                    try:
+                        cursor.execute(f"UPDATE politicians SET birth_date = '{dob}' WHERE id = {db_politician['id']}")
+                    except (Exception, Error) as error:
+                        print("Error while updating votes", error)
+                        connection.rollback()
+                    connection.commit()
+                if not db_politician['gender']:
+                    g = genders[politician['Płeć']]
+                    try:
+                        cursor.execute(f"UPDATE politicians SET gender = '{g}' WHERE id = {db_politician['id']}")
+                    except (Exception, Error) as error:
+                        print("Error while updating votes", error)
+                        connection.rollback()
+                    connection.commit()
+        if not exists:
+            g = genders[politician['Płeć']]
+            try:
+                cursor.execute(f"INSERT INTO politicians"
+                               f"(name, surname, profession, picture, education, birth_place, votes, birth_date, gender) VALUES"
+                               f"('{name}', '{surname}', '{profession}', decode('{picture.decode()}', 'base64'),"
+                               f"'{edu}', '{pob}', '{votes}', '{dob}', '{g}')")
+            except (Exception, Error) as error:
+                print("Error while updating votes", error)
+                connection.rollback()
+            connection.commit()
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        cursor.close()
+        connection.close()
+
+
+data = read_data()
+menage_data(data)
--- tools/data_collectors/goverment/representative_profiles/representative_normalizer.py
+++ tools/data_collectors/goverment/representative_profiles/representative_normalizer.py
@@ -1,34 +1,54 @@
 from representative_crawler import RepresentativeCrawler
-import datetime
+from datetime import datetime
 import json
+import base64
+import requests
+import re
 
 
 class RepresentativeNormalizer:
     def __init__(self):
+        self.data: list = []
+
+    def crawl_data(self):
         crawler = RepresentativeCrawler()
         self.data: list = crawler.get_data()
-        del crawler
-        self.normalize()
+
+    def get_data_from_file(self, file_name):
+        with open(file_name, 'r') as f:
+            self.data = json.load(f)
 
     def str_to_date(self, date_str: str):
-        print(datetime.datetime.strptime(date_str, '%d-%m-%Y'))
+        print(datetime.strptime(date_str, '%d-%m-%Y'))
 
     def normalize(self):
         for datum in self.data:
             self.normalize_name(datum)
+            self.normalize_pic(datum)
             self.normalize_elect_date(datum)
             self.normalize_period(datum)
-            self.normalize_dob_pob(datum)
+            self.normalize_yob_pob(datum)
+            self.normalize_speeches(datum)
 
     def normalize_name(self, representative: dict):
-        # 'Imię': 'Rafał', 'Nazwisko': 'Adamczyk'
+        """
+        Normalize day of birth and place of birth based on a string with sum of them.
+        """
         name = representative.pop('nazwa').split()
         representative['Imię'] = " ".join(name[0:-1])
         representative['Nazwisko'] = name[-1]
 
+    def normalize_pic(self, representative: dict):
+        """
+        Change value of picture(zdjęcie) from url to base64
+        """
+        if representative.get('zdjęcie'):
+            try:
+                representative['zdjęcie'] = base64.b64encode(requests.get(representative['zdjęcie']).content)
+            except Exception as e:
+                print(f"Cannot open {representative.get('zdjęcie')}.\n{e}")
+
     def normalize_elect_date(self, representative: dict):
-        # 'Płeć': 'męska'
-        # 'Dzień wybrania': '13-10-2019'
         if representative.get('Wybrany dnia'):
             representative['Płeć'] = 'męska'
             representative['Dzień wybrania'] = representative.pop('Wybrany dnia')
@@ -37,21 +57,30 @@ class RepresentativeNormalizer:
             representative['Dzień wybrania'] = representative.pop('Wybrana dnia')
 
     def normalize_period(self, representative: dict):
-        # 'Staż parlamentarny': ['poseł II kadencji', 'poseł III kadencji', 'poseł IV kadencji', 'poseł VI kadencji', 'poseł VII kadencji']
+        """
+        Split parliament period(Staż parlamentarny) and clean data.
+        """
         representative['Staż parlamentarny'] = [period.replace('poseł ', '').replace('kadencji', '') for period in
                                                 representative.get('Staż parlamentarny').split(', ')
                                                 if period != 'brak']
 
-    def normalize_dob_pob(self, representative: dict):
+    def normalize_yob_pob(self, representative: dict):
         """
-        Normalize day of birth and place of birth
+        Normalize date of birth(Data urodzenia) and place of birth(Miejsce urodzenia) based on a string with sum of them
+        (Data i miejsce urodzenia).
         """
         d = representative.pop('Data i miejsce urodzenia').split(', ')
         representative['Data urodzenia'], representative['Miejsce urodzenia'] = d[0], d[1]
 
+    def normalize_speeches(self, representative: dict):
+        if representative.get("Wypowiedzi"):
+            for speech in representative.get("Wypowiedzi"):
+                speech['tekst'] = "/n".join(re.sub(r'\(.*\)', '', text) for text in speech['tekst'] if re.sub(
+                    r'\(.*\)', '', text))
+
 
 if __name__ == '__main__':
     r = RepresentativeNormalizer()
-    d = r.data
-    with open('representative_fix.json', 'w') as fp:
-        json.dump(d, fp)
+    r.get_data_from_file('representative.json')
+    r.normalize()
+    print(r.data[0]['zdjęcie'])

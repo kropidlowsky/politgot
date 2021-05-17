8000aa197a591e9ee877cd030a01cf716441863d
dariusz-rodzik <60323554+dariusz-rodzik@users.noreply.github.com>
2021-05-17 19:11:11 +0200

    frontend/src/components/PartiesList:

--- frontend/src/components/PartiesList.tsx
+++ frontend/src/components/PartiesList.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const PartiesList = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([""]);
+  useEffect(() => {
+    setData(["partie", "lista"]);
+  }, [data]);
   return <h1>ListaParti</h1>;
 };
 export default PartiesList;

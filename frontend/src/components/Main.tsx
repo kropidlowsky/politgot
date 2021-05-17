96eeee8faf6bc26b31eda9d05f4d17042c5edb04
dariusz-rodzik <60323554+dariusz-rodzik@users.noreply.github.com>
2021-05-17 18:49:06 +0200

    Update Main.tsx

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -63,7 +63,7 @@ function TestmonialCard({
             {message}
           </chakra.p>
           <chakra.p fontWeight={"bold"} fontSize={14}>
-            {name + " x " + surname}
+            {name + " x  x x x" + surname}
             <chakra.span fontWeight={"medium"} color={"gray.500"}>
               {" "}
               - {date}

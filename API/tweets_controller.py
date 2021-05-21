e7249fbecef573a15316611a912271df0436dd90
PumpkinHead444 <48615982+3662149@users.noreply.github.com>
2021-05-21 12:16:46 +0200

    Update tweets_controller.py

--- API/tweets_controller.py
+++ API/tweets_controller.py
@@ -11,7 +11,7 @@ import psycopg2
 HEADERS = {'Access-Control-Allow-Origin': '*',
            'Access-Control-Allow-Credentials': True,
            'Access-Control-Allow-Methods': 'HEAD, GET, OPTIONS',
-           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin'
+           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin, Origin, access-control-allow-origin'
            }
 
 app = Flask(__name__)
